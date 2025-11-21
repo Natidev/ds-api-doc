e4a0128a982564bf396965c374d8b81fba63f2a7
Natidev <53361365+Natidev@users.noreply.github.com>
2025-11-22 00:13:47 +0300

    Final fix

--- config.js
+++ config.js
@@ -1,24 +1,13 @@
 const YAML = require('yamljs');
 const path = require('path');
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yml'));
 
 // Custom Swagger options
 const swaggerOptions = {
-  customCss: `
-    .swagger-ui .topbar { display: none }
-    .swagger-ui .info { margin: 20px 0; }
-    .swagger-ui .scheme-container { background: #fafafa; }
-  `,
-  customSiteTitle: "My Express API Documentation",
-  swaggerOptions: {
-    persistAuthorization: true,
-    displayRequestDuration: true,
-    filter: true,
-    tryItOutEnabled: true,
-    defaultModelsExpandDepth: 2,
-    defaultModelExpandDepth: 2
-  }
+    customCss:'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
 };
 
 module.exports = {
